b0fc60c93fc9042176c23a8a8ab59d28b2370580
Mael BEAUJARD <mbeaujar@e1r12p6.42.fr>
2021-06-14 16:01:22 +0200

    fix

--- source/engine/sprites.c
+++ source/engine/sprites.c
@@ -6,13 +6,13 @@
 /*   By: mbeaujar <mbeaujar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 15:24:13 by mbeaujar          #+#    #+#             */
-/*   Updated: 2021/06/14 15:21:18 by mbeaujar         ###   ########.fr       */
+/*   Updated: 2021/06/14 16:00:35 by mbeaujar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	draw_texture(t_game *game, int i, int j, t_image image)
+void	draw_texture(t_game *game, int i, int j, t_image img)
 {
 	int		x;
 	int		y;
@@ -26,9 +26,9 @@ void	draw_texture(t_game *game, int i, int j, t_image image)
 		y = 0;
 		while (y < game->sq_leny)
 		{
-			txt_x = image.width / (100 / ((double)x / game->sq_lenx) * 100.0);
-			txt_y = image.height / (100 / ((double)y / game->sq_leny) * 100.0);
-			color = image.addr + ((4 * image.width * txt_y) + (4 * txt_x));
+			txt_x = img.width / (100 / (((double)x / game->sq_lenx) * 100.0));
+			txt_y = img.height / (100 / (((double)y / game->sq_leny) * 100.0));
+			color = img.addr + ((4 * img.width * txt_y) + (4 * txt_x));
 			if (*(unsigned int *)color != NONE)
 				my_mlx_pixel_put((j * game->sq_lenx) + x,
 					(i * game->sq_leny) + y, game, *(unsigned int *)color);
